beadd22858c60f61c62d4051e8bce5ad3db9d744
Santanu Halder <santanu.halder@rplanx.com>
2019-03-06 23:37:33 +0530

    added comment

--- src/main/java/com/training/MainApp.java
+++ src/main/java/com/training/MainApp.java
@@ -5,6 +5,8 @@ import com.training.util.ReadCSVUtil;
 public class MainApp {
 
 	public static void main(String[] args) {
+		
+		// Sample CSV Read
 		ReadCSVUtil util = new ReadCSVUtil();
 		util.sampleRead();
 	}
